f98907b205e756c39003b82d14f0afcb918a25dd
le dung <dungle.dev2412@gmail.com>
2022-11-05 10:01:40 +0700

    lesson 5

--- lesson-4/src/App.js
+++ lesson-4/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <RegisterPage />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>HomePage</h1>} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
@@ -36,9 +26,9 @@ class RegisterPage extends React.Component {
       password: "",
       passwordConfirm: "",
       errorMessage: {
-        fullname: "",
-        username: "",
-        password: "",
+        fullnameM: "",
+        usernameM: "",
+        passwordM: "",
         passwordConfirmM: "",
       }
     }
@@ -53,6 +43,9 @@ class RegisterPage extends React.Component {
     } else {
       errorMessage['passwordConfirmM'] = "";
     }
+    if (nameInput == "username") {
+      this.checkUsernameExist(value);
+    }
     this.setState({
       ...this.state,
       [nameInput]: value,
@@ -60,21 +53,57 @@ class RegisterPage extends React.Component {
     })
   }
 
+  checkUsernameExist(valueInput) {
+    fetch(`https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users?username=${valueInput}`, {
+      method: "GET"
+    }).then((response) => response.json()).then((users) => {
+      if (users.filter((user) =>
+        user.username == valueInput
+      ).length > 0) {
+        this.setState({
+          ...this.state,
+          errorMessage: {
+            ...this.state.errorMessage,
+            usernameM: "Username da ton tai",
+          },
+        })
+      } else {
+        this.setState({
+          ...this.state,
+          errorMessage: {
+            ...this.state.errorMessage,
+            usernameM: "",
+          },
+        })
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   handleSubmitForm = (e) => {
     e.preventDefault();
+
     const { errorMessage, fullname, password, username } = this.state
     if (Object.values(errorMessage).filter((value) => value != "").length > 0) {
       return;
     }
     fetch("https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users", {
-      method: "POST",
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
-        user: {
-          name: fullname,
-          username: username,
-          password: password,
-        }
+        fullname: fullname,
+        username: username,
+        password: password,
       })
+    }).then(() => {
+      // window.location.href = "/login";
+      const navigate = useNavigate();
+      navigate('/login')
+    }).catch((err) => {
+
     });
   }
 
@@ -94,6 +123,7 @@ class RegisterPage extends React.Component {
           <input type="text" name="username" onChange={(e) => {
             this.onChangeInput("username", e.target.value);
           }} />
+          {errorMessage.usernameM != "" ? <div>{errorMessage.usernameM}</div> : <></>}
         </div>
         <div>
           <label>Password</label>
@@ -107,6 +137,93 @@ class RegisterPage extends React.Component {
             this.onChangeInput("passwordConfirm", e.target.value);
           }} />
           {errorMessage.passwordConfirmM != "" ? <div>{errorMessage.passwordConfirmM}</div> : <></>}
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </>
+  }
+}
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: "",
+    }
+  }
+
+  onChangeInput = (nameInput, value) => {
+    // const errorMessage = {
+    //   ...this.state.errorMessage,
+    // }
+    // if (nameInput == "passwordConfirm" && this.state.password != value) {
+    //   errorMessage['passwordConfirmM'] = "Password ko giong nhau";
+    // } else {
+    //   errorMessage['passwordConfirmM'] = "";
+    // }
+    // if (nameInput == "username") {
+    //   this.checkUsernameExist(value);
+    // }
+    // this.setState({
+    //   ...this.state,
+    //   [nameInput]: value,
+    //   errorMessage: errorMessage,
+    // })
+  }
+
+  checkUsernameExist(valueInput) {
+    fetch(`https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users?username=${valueInput}`, {
+      method: "GET"
+    }).then((response) => response.json()).then((users) => {
+      if (users.filter((user) =>
+        user.username == valueInput
+      ).length > 0) {
+        this.setState({
+          ...this.state,
+          errorMessage: {
+            ...this.state.errorMessage,
+            usernameM: "Username da ton tai",
+          },
+        })
+      } else {
+        this.setState({
+          ...this.state,
+          errorMessage: {
+            ...this.state.errorMessage,
+            usernameM: "",
+          },
+        })
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  handleSubmitForm = (e) => {
+
+  }
+
+  render() {
+    const { errorMessage } = this.state
+    return <>
+      <h1>Login</h1>
+      <form onSubmit={this.handleSubmitForm}>
+        <div>
+          <label>Username</label>
+          <input type="text" name="username" onChange={(e) => {
+            this.onChangeInput("username", e.target.value);
+          }} />
+          {errorMessage.usernameM != "" ? <div>{errorMessage.usernameM}</div> : <></>}
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" onChange={(e) => {
+            this.onChangeInput("password", e.target.value);
+          }} />
         </div>
         <div>
           <input type="submit" />
