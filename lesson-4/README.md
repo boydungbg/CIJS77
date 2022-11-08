6037dd64dd66867a012125361c599e4fb4bb9489
le dung <dungle.dev2412@gmail.com>
2022-11-08 22:11:54 +0700

    lesson 7

--- lesson-4/README.md
+++ lesson-4/README.md
@@ -1,4 +1,7 @@
-Bai tap ve nha
-- Validate Full name, username, password
-- get user, check xem username đã tồn tại hay chưa
-- css register
+- Tạo thêm một trang là edit-photo
+khi chọn ảnh thì chuyển tới trang edit-photo
+lấy thông tin của ảnh và fill vào ô input
+khi save lại thì update thông tin ảnh đấy
+VD: /edit-photo/{id của ảnh}
+Trong document react-route-dom
+- Làm thêm preview ảnh khi input ảnh.
--- lesson-4/src/App.js
+++ lesson-4/src/App.js
@@ -6,26 +6,32 @@ import {
   Route,
   withRouter,
   useHistory,
+  Redirect,
 } from "react-router-dom";
+import { useContext } from 'react';
+
+let UserContext = React.createContext();
 
 function App() {
+  let userId = localStorage.getItem("userId");
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {/* <HomePage /> */}
-          <HomePageHook />
-        </Route>
-        <Route exact path="/register"  >
-          <RegisterPage />
-        </Route>
-        <Route exact path="/login" >
-          <LoginPage />
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={{ userId: JSON.parse(userId) }}>
+      <Router>
+        <Switch>
+          <Route exact path="/register"  >
+            <RegisterPage />
+          </Route>
+          <Route exact path="/login" >
+            <LoginPage />
+          </Route>
+          <PrivateRoute exact component={HomePageHook} path="/" />
+          <PrivateRoute exact component={AddPhotoPage} path="/add-photos" />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
+
 
 
 export default App;
@@ -92,7 +98,6 @@ class HomePage extends React.Component {
 HomePage = withRouter(HomePage);
 
 function HomePageHook() {
-  let history = useHistory();
   let [listImage, setListImage] = useState([]);
   let [page, setPage] = useState(1);
   let [limit, setLimit] = useState(10);
@@ -286,58 +291,42 @@ class LoginPage extends React.Component {
   }
 
   onChangeInput = (nameInput, value) => {
-    // const errorMessage = {
-    //   ...this.state.errorMessage,
-    // }
-    // if (nameInput == "passwordConfirm" && this.state.password != value) {
-    //   errorMessage['passwordConfirmM'] = "Password ko giong nhau";
-    // } else {
-    //   errorMessage['passwordConfirmM'] = "";
-    // }
-    // if (nameInput == "username") {
-    //   this.checkUsernameExist(value);
-    // }
-    // this.setState({
-    //   ...this.state,
-    //   [nameInput]: value,
-    //   errorMessage: errorMessage,
-    // })
+    this.setState({
+      ...this.state,
+      [nameInput]: value,
+    })
   }
 
-  checkUsernameExist(valueInput) {
-    fetch(`https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users?username=${valueInput}`, {
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+    fetch(`https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users?username=${this.state.username}`, {
       method: "GET"
     }).then((response) => response.json()).then((users) => {
-      if (users.filter((user) =>
-        user.username == valueInput
-      ).length > 0) {
-        this.setState({
-          ...this.state,
-          errorMessage: {
-            ...this.state.errorMessage,
-            usernameM: "Username da ton tai",
-          },
-        })
+      let userFound = users.find((user) =>
+        user.username === this.state.username
+      )
+      let message = ''
+      if (userFound != null) {
+        if (this.state.username == userFound.username && userFound.password === this.state.password) {
+          localStorage.setItem("userId", JSON.stringify(userFound.id));
+          this.props.history.push("/");
+        } else {
+          message = "Sai username hoac password"
+        }
       } else {
-        this.setState({
-          ...this.state,
-          errorMessage: {
-            ...this.state.errorMessage,
-            usernameM: "",
-          },
-        })
+        message = "Sai username hoac password"
       }
+      this.setState({
+        ...this.state,
+        password: "",
+        errorMessage: message
+      })
     }).catch((error) => {
       console.log(error);
     });
   }
 
-  handleSubmitForm = (e) => {
-
-  }
-
   render() {
-    const { errorMessage } = this.state
     return <>
       <h1>Login</h1>
       <form onSubmit={this.handleSubmitForm}>
@@ -346,7 +335,6 @@ class LoginPage extends React.Component {
           <input type="text" name="username" onChange={(e) => {
             this.onChangeInput("username", e.target.value);
           }} />
-          {errorMessage.usernameM != "" ? <div>{errorMessage.usernameM}</div> : <></>}
         </div>
         <div>
           <label>Password</label>
@@ -354,10 +342,81 @@ class LoginPage extends React.Component {
             this.onChangeInput("password", e.target.value);
           }} />
         </div>
+        {this.state.errorMessage != '' ? <div>{this.state.errorMessage}</div> : <></>}
         <div>
-          <input type="submit" />
+          <button >Login</button>
         </div>
       </form>
     </>
   }
 }
+
+LoginPage = withRouter(LoginPage);
+
+
+function PrivateRoute({ component: Component, path, ...rest }) {
+  let userId = localStorage.getItem("userId");
+  return <Route {...rest}
+    render={(props) => {
+      return userId != null && userId != "" ? <Component {...props} /> : <Redirect to={{
+        pathname: "/login"
+      }} />
+    }}>
+  </Route>
+}
+
+function AddPhotoPage() {
+  let userContext = useContext(UserContext);
+  let history = useHistory();
+  let [input, setInput] = useState({
+    image: "",
+    description: "",
+  })
+
+  const onChangeInput = (nameInput, value) => {
+    setInput({
+      ...input,
+      [nameInput]: value
+    })
+  }
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    fetch(`https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users/${userContext.userId}/photos`, {
+      method: "POST",
+      body: JSON.stringify({
+        userId: userContext.userId,
+        image: input.image,
+        description: input.description
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((res) => {
+      if (res.status == 201) {
+        setInput({
+          image: "",
+          description: "",
+        })
+        history.push("/");
+      }
+    })
+  }
+
+  return <form onSubmit={handleSubmitForm}>
+    <div>
+      <label>Image</label>
+      <input type="text" name="image" onChange={(e) => {
+        onChangeInput("image", e.target.value);
+      }} />
+    </div>
+    <div>
+      <label>Description</label>
+      <textarea type="description" name="description" onChange={(e) => {
+        onChangeInput("description", e.target.value);
+      }} />
+    </div>
+    <button>Them photo</button>
+  </form>
+}
+
